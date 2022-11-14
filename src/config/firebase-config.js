fe11307f0f1d73d9df5dc577b08348d6ba285a71
shoyab01 <shaikshoyab01@gmail.com>
2022-11-14 08:37:45 +0530

    added firebase config

---
+++ src/config/firebase-config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAYF5g8ab-YYePP2FFwmdkXVUipNACOyvU",
+    authDomain: "alumni-377ae.firebaseapp.com",
+    projectId: "alumni-377ae",
+    storageBucket: "alumni-377ae.appspot.com",
+    messagingSenderId: "74688613441",
+    appId: "1:74688613441:web:a2c54d40ca6d3e4d064ac0"
+};
+
+firebase.initializeApp(firebaseConfig);
